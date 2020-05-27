e15d9851dc538265e5c4516e984bc083422b3183
JoseOgam <jotienoochieng@gmail.com>
2020-05-27 10:05:16 +0300

    initial commits

---
+++ LinkedList.js
@@ -0,0 +1,9 @@
+const LinkedListNode = require('./LinkedListNode');
+class LinkedList {
+    constructor() {
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+    }
+}
+module.exports = LinkedList
---
+++ index.js
@@ -0,0 +1,6 @@
+const LinkedListNode = require('./LinkedListNode');
+
+console.log(new LinkedListNode(1));
+console.log(new LinkedListNode('A', 'B'));
+console.log(new LinkedListNode('string', true));
+console.log(new LinkedListNode('oseph', 'true'));
